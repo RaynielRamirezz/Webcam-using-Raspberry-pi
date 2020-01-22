42fe4306702d683f98eda4668f1f63363755aaff
RaynielRamirezz <59105007+RaynielRamirezz@users.noreply.github.com>
2020-01-22 12:15:10 -0400

    Add files via upload

---
+++ newonecv2TEST1.py
@@ -0,0 +1,98 @@
+import numpy as np
+import os
+import cv2
+import time
+
+
+filename = 'video.avi'
+filename1 = '1video.avi'
+
+# Set resolution for the video capture
+# Function adapted from https://kirr.co/0l6qmh
+def change_res(cap, width, height):
+    cap.set(3, width)
+    cap.set(4, height)
+
+# Standard Video Dimensions Sizes
+STD_DIMENSIONS =  {
+    "480p": (640, 480),
+    "720p": (1280, 720),
+    "1080p": (1920, 1080),
+    "4k": (3840, 2160),
+}
+
+
+# grab resolution dimensions and set video capture to it.
+def get_dims(cap, res='1080p'):
+    width, height = STD_DIMENSIONS["480p"]
+    if res in STD_DIMENSIONS:
+        width,height = STD_DIMENSIONS[res]
+    ## change the current caputre device
+    ## to the resulting resolution
+    change_res(cap, width, height)
+    return width, height
+
+# Video Encoding, might require additional installs
+# Types of Codes: http://www.fourcc.org/codecs.php
+VIDEO_TYPE = {
+    'avi': cv2.VideoWriter_fourcc(*'XVID'),
+    #'mp4': cv2.VideoWriter_fourcc(*'H264'),
+    'mp4': cv2.VideoWriter_fourcc(*'XVID'),
+}
+
+def get_video_type(filename):
+    filename, ext = os.path.splitext(filename)
+    if ext in VIDEO_TYPE:
+      return  VIDEO_TYPE[ext]
+    return VIDEO_TYPE['avi']
+
+
+
+cap = cv2.VideoCapture(0)
+out = cv2.VideoWriter(filename, get_video_type(filename), 10, get_dims(cap, '480p'),isColor=False)
+start_time = time.time()
+
+
+while True:
+    #ret, frame = cap.read()
+    #out.write(frame)
+    #cv2.imshow('frame',frame)
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    out.write(gray)
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+    end_time = time.time()
+    elapsed = end_time - start_time
+    if int(elapsed) > 20:
+        break
+
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
+
+
+cap = cv2.VideoCapture(0)
+out = cv2.VideoWriter(filename1, get_video_type(filename1), 10, get_dims(cap, '480p'),isColor=False)
+start_time = time.time()
+
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    out.write(gray)
+    cv2.imshow('frame1',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+    end_time = time.time()
+    elapsed = end_time - start_time
+    if int(elapsed) > 20:
+        break
+
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
